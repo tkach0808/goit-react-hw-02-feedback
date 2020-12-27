f6d9637b791e28bf7847af27021122eb4d4cef7d
tkach0808 <tkach0808@gmail.com>
2020-12-28 01:33:11 +0200

    Updates

---
+++ precache-manifest.6ea3d6caabd4cf7f721df21c811b2645.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40bb494faef674dbbe6f7e48fe048573",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "cc538c58862a51c40667",
+    "url": "/goit-react-hw-02-feedback/static/css/main.febda7cc.chunk.css"
+  },
+  {
+    "revision": "4ea0b068f91ef526bbdc",
+    "url": "/goit-react-hw-02-feedback/static/js/2.e57047ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.e57047ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc538c58862a51c40667",
+    "url": "/goit-react-hw-02-feedback/static/js/main.4b29628e.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
